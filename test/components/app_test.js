cc40f7431b2410595bc21135ba9e9cb6c4b59c4c
StevanCakic <stevancakic93@gmail.com>
2017-09-30 16:47:59 +0200

    simulate some events for testing comment textarea

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,12 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import { renderComponent, expect } from "../test_helper";
+import App from "../../src/components/app";
 
-describe('App' , () => {
-
-
-  it('shows the correct text', () => {
-
-    let component = renderComponent(App);
-
-    expect(component).to.contain("React simple starter");
-    
+describe("App", () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+  it("shows a comment box", () => {
+    expect(component.find(".comment-box")).to.exist;
   });
 });
