f6e174157ddfb59572dc6ce47b8ed1de2597c5ba
StevanCakic <stevancakic93@gmail.com>
2017-09-30 15:24:57 +0200

    create comment component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,13 +2,13 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shows the correct text', () => {
+
+    let component = renderComponent(App);
+
+    expect(component).to.contain("React simple starter");
+    
   });
 });
